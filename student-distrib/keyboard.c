ac96b9966965567ed2063c5d0ce821645499fecc
Faust <mhfaust2@illinois.edu>
2016-10-16 02:33:33 -0500

    need to fill keyboard tables and set up output, otherwise most of the skeleton seems done probably supply additional commenting

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,6 +1,25 @@
 #include "keyboard.h"
 
 /*
+ * get_char
+ * DESCRIPTION: Retrieves character from control data port
+ * INPUT: none.
+ * OUTPUTS: character present in control data port
+ * RETURN VALUE: none.
+ * SIDE EFFECTS: Initializes the keyboard
+*/
+char get_char()
+{
+	char a = 0;
+	while(1){
+		if(a != inb(CONTROL_DATA_PORT)){
+			a = inb(CONTROL_DATA_PORT);
+			if(a != 0)
+				return a;
+		}
+	}
+}
+/*
  * init_kbd
  * DESCRIPTION: Initializes the connected PS2 keyboard on boot-up.
  * INPUT: none.
@@ -10,9 +29,50 @@
 */
 void init_kbd()
 {
-	// Resources: http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
-	// http://www.computer-engineering.org/ps2keyboard/
-	// Assume that there's no USB conflict and that the PS2 controller exists
-
-	// Disable PS2 devices first so they don't send data and mess up initialization
+	outb(MAKE_RELEASE_WORD, KEYBOARD_PORT);
+}
+/*
+ * display_char
+ * DESCRIPTION: returns an unsigned long containing characters to display in the two most significant bits
+ *				in the 3 least significant bits returns the (potentially) updated state
+				1xx control is on, x1x capslock is on, xx1 shift is on
+ * INPUT: state indicating current state of shift, capslock, and control
+ * OUTPUTS: none.
+ * RETURN VALUE: none.
+ * SIDE EFFECTS: Initializes the keyboard
+*/
+unsigned long display_char(unsigned long state)	//shouldn't need to keep track of prev char
+{
+	unsigned long flags;  //make critical so states are able remain correct
+	cli_and_save(flags); // save flags and halt interupts for critical section
+	char a = get_char();
+	unsigned long temp = a;
+	return temp;
+	/*determines map to use either controlmap, shiftmap, or normal map
+	control has highest priority, and therefore goes to control map
+	*/
+	unsigned long selected_map = choose_map[state];
+	switch(a){
+		case (CAPS_LOCK_PRESS):
+			return (state ^ CAP_LOCK_STATE)
+		case (CAPS_LOCK_RELEASE):	//dont care about this, return current state with null
+			return state;		
+		case (SHIFT_PRESS):
+			return (state ^ SHIFT_STATE)
+		case (SHIFT_RELEASE):
+			return (state ^ SHIFT_STATE)
+		case (CTL_PRESS):
+			return (state ^ CTL_STATE)
+		case (CTL_RELEASE):
+			return (state ^ CTL_STATE) 
+			//assuming nothing goes wrong for ctl and shift press sets corresponding state bits to 1
+			//release sets corresponding state bits to 0
+		default:
+			char adjusted = (keyboard_map_select[selected_map])[a];
+			unsigned long adjusted_char = adjusted << CHAR_SHIFT; //
+			//adjusted_char += state; if not printing in display
+			// either print here or return char press encoded in updated state
+			return state; //or adjusted_state
+	}
+	restore_flags(flags);	// restore our flags
 }
--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -1,6 +1,33 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
+#define KEYBOARD_PORT 0x21
+#define MAKE_RELEASE_WORD 0xFC
+#define CONTROL_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x60
+#define CAPS_LOCK_PRESS 0x00
+#define CAPS_LOCK_RELEASE 0x00
+#define SHIFT_PRESS 0x00
+#define SHIFT_RELEASE 0x00
+#define CTL_PRESS 0x00
+#define CTL_RELEASE 0x00
+#define CHAR_SHIFT 6
+#define CTL_MAP 2
+#define SHIFT_MAP 1
+#define NORMAL_MAP 0
+#define CAP_LOCK_STATE 0x02
+#define SHIFT_STATE 0x01
+#define CTL_STATE 0x04
+#define NUMB_KEYS 128
+#define NUMB_MAPS 3
+#define NUMB_STATES 8
 
+unsigned long choose_map[NUMB_STATES] = {NORMAL_MAP, SHIFT_MAP, SHIFT_MAP, NORMAL_MAP, CTL_MAP, CTL_MAP, CTL_MAP, CTL_MAP};
+unsigned char keyboard_normalmap[NUMB_KEYS] = {}; 
+unsigned char keyboard_shiftmap[NUMB_KEYS] =  {}; 
+unsigned char keyboard_controlmap[NUMB_KEYS] = { }; 
+unsigned char* keyboard_map_select[NUMB_MAPS] = {keyboard_normalmap, keyboard_shiftmap, keyboard_controlmap};
+extern char display_char(unsigned long state);
 extern void init_kbd();
+char get_char();
 
 #endif
