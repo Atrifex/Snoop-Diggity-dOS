2d45e2be9cb4dc9fe3de6bb4b74364f2638f19fb
Rishi Thakkar <rrthakk2@illinois.edu>
2016-10-15 20:55:11 -0500

    saving

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -10,7 +10,7 @@
 #define RTC_CREG_C 0x8C
 #define ENABLE_PERIODIC_INT 0x40
 #define MAINTAIN_SIG_BYTE 0xF0
-#define DESIRED_PIE_RATE 0x02
+#define DESIRED_PIE_RATE 0x0F
 #define RTC_LINE_NO 8
 
 // registers declared with nonmaskable interrupts
@@ -27,22 +27,19 @@
 # RETURN VALUE: none
 # REGISTER USAGE: We save all registers and modify control registers A and B of the RTC
 */
-
-void init_rtc(){
+void init_rtc() 
+{
 	unsigned long flags; 
 	cli_and_save(flags); // save flags and halt interupts for critical section
-	outb(RTC_CONTROL_PORT, RTC_CREG_B); // selects control register B of the RTC
-	io_wait();
+	outb(RTC_CREG_B, RTC_CONTROL_PORT); // selects control register B of the RTC
 	char portdata = inb(RTC_DATA_PORT) | ENABLE_PERIODIC_INT; //get current state and set the disired rate
-	io_wait();
-	outb(RTC_DATA_PORT, portdata); //update register B with PIE enabled
-	io_wait();
-	outb(RTC_CONTROL_PORT, RTC_CREG_A); // selects control register A of the RTC
-	io_wait();
+	outb(RTC_CREG_B, RTC_CONTROL_PORT); // selects control register B of the RTC
+	outb(portdata, RTC_DATA_PORT); //update register B with PIE enabled
+
+	outb(RTC_CREG_A, RTC_CONTROL_PORT); // selects control register A of the RTC
 	portdata = (inb(RTC_DATA_PORT) & MAINTAIN_SIG_BYTE) | DESIRED_PIE_RATE; //Set interrupt handle rate to 2 while maintaining significant bits
-	io_wait();
-	outb(RTC_DATA_PORT, portdata); //updatew register B with PIE enabled
-	io_wait();
+	outb(RTC_CREG_A, RTC_CONTROL_PORT); // selects control register A of the RTC
+	outb(portdata, RTC_DATA_PORT); //updatew register B with PIE enabled
 	restore_flags(flags);	// restore our flags
 
 	enable_irq(RTC_LINE_NO);
