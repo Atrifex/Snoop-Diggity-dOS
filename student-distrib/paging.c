8688e94c418366bf1502ca1b4dd044567e471c3b
Rishi Thakkar <rrthakk2@illinois.edu>
2016-10-16 01:41:58 -0500

    Paging enabled.

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,18 +1,46 @@
 #include "paging.h"
+#include "lib.h"
+
+pde_t page_directory[PD_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
+pte_t page_table[PT_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
 
 /*
- * init_kbd
- * DESCRIPTION: Initializes the connected PS2 keyboard on boot-up.
+ * init_paging
+ * DESCRIPTION: .
  * INPUT: none.
  * OUTPUTS: none.
  * RETURN VALUE: none.
- * SIDE EFFECTS: Initializes the keyboard
+ * SIDE EFFECTS: allows paging
 */
 void init_paging()
 {
-	// Resources: http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
-	// http://www.computer-engineering.org/ps2keyboard/
-	// Assume that there's no USB conflict and that the PS2 controller exists
+	// set up first 4MB (video memory) - page_directory entry 0
+	init_video_memory();
 
-	// Disable PS2 devices first so they don't send data and mess up initialization
+	// set up second 4MB (kernel-space memory) - page_directory entry 1
+	
+	// set up 8MB to 4GB - reset of page_directory
+
+	// enable paging
+	paging_hw_enable(page_directory);
+}
+
+
+void init_video_memory()
+{
+	int i;
+
+
+
+	for(i=0; i < PT_ENTRIES; i++)
+	{
+		if(i*BYTES_TO_ALIGN_TO == 0xB8000)
+		{
+			page_table[i] = PDE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO)
+		}	
+		else
+		{
+			page_table[i] = PDE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO)
+		}	
+	}
 }
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,6 +1,28 @@
 #ifndef PAGING_H
 #define PAGING_H
 
+// base address of kernel in both physical and virtual memory
+#define KERNEL_BASE_ADDRESS 0x400000
+
+#define PDE_ADDRESS(directory_entry) (directory_entry & (0xFFFFF000))
+#define PTE_ADDRESS(table_entry) (table_entry & (0xFFFFF000))
+
+#define PDE_ADDRESS_ASSIGN(address) (address & (0xFFFFF000))
+#define PTE_ADDRESS_ASSIGN(address) (address & (0xFFFFF000))
+
+#define PD_ENTRIES 1024
+#define PT_ENTRIES 1024
+#define BYTES_TO_ALIGN_TO 4096
+
+typedef unsigned long pde_t;
+typedef unsigned long pte_t;
+
+extern pde_t page_directory[PD_ENTRIES];
+extern pte_t page_table[PT_ENTRIES];
+
 extern void init_paging();
+
+// written in paging_asm.S
+extern void paging_hw_enable(pde_t* base);
 
 #endif
