38d423308ff7cf80424b1b02087394ac98897410
David Desberg <david+github@daviddesberg.com>
2016-11-05 17:39:54 -0500

    paging prep for userland tasks

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,20 +1,25 @@
 #include "paging.h"
 #include "lib.h"
 
-/* declerations of kernel page_directory and page_table */
-pde_t page_directory[PD_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
-pte_t page_table[PT_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
+/* declarations of kernel kernel_page_directory and kernel_page_table */
+pde_t kernel_page_directory[PD_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
+pte_t kernel_page_table[PT_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
+
+/* per-program page directories - for now, one shell, one user level program - so only use entries 0 and 1 - but more in future*/
+pde_t user_level_page_directories[MAX_TASKS][PD_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
+/* per-program base page tables i.e. these map 0MB-4MB, marking everything as not present but video memory, and setting video memory to supervisor only */
+pte_t user_level_base_page_tables[MAX_TASKS][PT_ENTRIES] __attribute__((aligned(BYTES_TO_ALIGN_TO)));
 
 
 /* local functions */
-void init_first_4MB();
-void init_kernel_memory();
-void init_rest_of_memory();
+void init_kernel_pages();
+void setup_base_page_table(pte_t* base_page_table);
+void setup_task_paging(pde_t* page_directory, pte_t* base_page_table, uint32_t phys_program_address);
 
 
 /*
  * void init_paging()
- * DESCRIPTION: sets up paging and initializes the page descriptor table for the kernel.
+ * DESCRIPTION: sets up paging and initializes the page directory and singular page table for the kernel.
  * INPUT: none.
  * OUTPUTS: none.
  * RETURN VALUE: none.
@@ -22,79 +27,101 @@ void init_rest_of_memory();
 */
 void init_paging()
 {
-	// set up first 4MB (video memory) - page_directory entry 0
-	init_first_4MB();
-
-	// set up second 4MB (kernel-space memory) - page_directory entry 1
-	init_kernel_memory();
-
-	// set up 8MB to 4GB - reset of page_directory
-	init_rest_of_memory();
+	// set up kernel page directory/table
+	init_kernel_pages();
 
 	// enable paging
-	paging_hw_enable(page_directory);
+	paging_hw_enable(kernel_page_directory);
 }
 
 /*
- * void init_first_4MB()
- * DESCRIPTION: Initializes page descriptor entries to not present for 0MB to 4MB.
- *				This function also initializes the page table that the page_directory
- * 				points to and sets up the video memory portion of memory as present.
- * INPUT: none.
- * OUTPUTS: none.
- * RETURN VALUE: none.
- * SIDE EFFECTS: initializes page descriptor
+ * void init_kernel_pages()
+ * DESCRIPTION: Initializes the kernel's page directory and single page table (which maps 0MB-4MB)
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: initializes kerneL_page_directory and kernel_page_table
 */
-void init_first_4MB()
+void init_kernel_pages()
 {
 	int i;
 
-	page_directory[0] = PDE_ADDRESS_ASSIGN(page_table);
-	page_directory[0] = page_directory[0] | PDE_ENTRY_PRESENT;
+	// maps 0MB-4MB to the page table we set up below
+	kernel_page_directory[0] = PDE_ADDRESS_ASSIGN(kernel_page_table);
+	kernel_page_directory[0] = kernel_page_directory[0] | PDE_ENTRY_PRESENT;
 
-	for(i=0; i < PT_ENTRIES; i++)
-	{
-		if(i*BYTES_TO_ALIGN_TO == VIDEO_MEM_BASE)
-		{
-			page_table[i] = PTE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO);
-			page_table[i] = page_table[i] | PTE_ENTRY_PRESENT;
-		}
-		else
-		{
-			page_table[i] = PTE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO);
-			page_table[i] = page_table[i] | PTE_ENTRY_NOT_PRESENT;
+	// set up the page table which describes 0MB-4MB
+	setup_base_page_table(kernel_page_table);
+
+	// map 4MB-8MB to the kernel as a single 4MB page
+	kernel_page_directory[KERNEL_PDE_ENTRY] = PDE_ADDRESS_ASSIGN(KERNEL_BASE_ADDRESS);
+	kernel_page_directory[KERNEL_PDE_ENTRY] = kernel_page_directory[KERNEL_PDE_ENTRY] | PDE_ENTRY_4MB_PRESENT;
+
+	// map 8MB-4GB as not present
+	for(i = NOT_PRESENT_MEMORY_START; i < PD_ENTRIES; i++) {
+		kernel_page_directory[i] = PDE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_4MB);
+		kernel_page_directory[i] = kernel_page_directory[i] | PDE_ENTRY_NOT_PRESENT;
+	}
+}
+
+
+/*
+ * void setup_task_paging(pde_t* page_directory, pte_t* base_page_table, uint32_t phys_program_address)
+ * DESCRIPTION: Sets up a page directory for a new user-space process.
+ * INPUTS: page_directory: pointer to page directory for this process to initialize, base_page_table: pointer to base (0MB-4MB) page table for this process, phys_program_address: physical memory address to map program image to
+ * OUTPUTS: page_directory, base_page_table
+ * RETURN VALUE: none
+ * SIDE EFFECTS: updates passed params
+*/
+void setup_task_paging(pde_t* page_directory, pte_t* base_page_table, uint32_t phys_program_address)
+{
+	int i;
+
+	// we are not setting the user bit for this entry, so it will be inaccessible except for the kernel. tihs is what we want.
+	// as accessing video memory at VIDEO_MEM_BASE should only be done by the kernel, vidmap will enable userspace level video memory accesss
+	page_directory[0] = PDE_ADDRESS_ASSIGN(base_page_table);
+	page_directory[0] |= PDE_ENTRY_PRESENT;
+	
+	// setup the base page table mapping 0MB-4MB as not present except for video memory
+	setup_base_page_table(base_page_table);
+
+	// map 4MB-8MB to the kernel as a single 4MB page
+	page_directory[KERNEL_PDE_ENTRY] = PDE_ADDRESS_ASSIGN(KERNEL_BASE_ADDRESS);
+	page_directory[KERNEL_PDE_ENTRY] = page_directory[KERNEL_PDE_ENTRY] | PDE_ENTRY_4MB_PRESENT;
+
+	// map the program image at 0x08000000 and mark everything else as not present
+	for(i = NOT_PRESENT_MEMORY_START; i < PD_ENTRIES; i++) {
+		if(i*BYTES_TO_ALIGN_4MB == TASK_VIRTUAL_BASE_ADDRESS) {
+			page_directory[i] = PDE_ADDRESS_ASSIGN(phys_program_address);
+			page_directory[i] = page_directory[i] | PDE_ENTRY_4MB_PRESENT | PDE_USER_ACCESSIBLE;
+		} else {
+			page_directory[i] = PDE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_4MB);
+			page_directory[i] = page_directory[i] | PDE_ENTRY_NOT_PRESENT;
 		}
 	}
 }
 
-/*
- * void init_kernel_memory()
- * DESCRIPTION: initializes page descriptor entries to not present for 4MB to 8MB
- * INPUT: none.
- * OUTPUTS: none.
- * RETURN VALUE: none.
- * SIDE EFFECTS: initializes page descriptor
-*/
-void init_kernel_memory()
-{
-	page_directory[KERNEL_PDE_ENTRY] = PDE_ADDRESS_ASSIGN(KERNEL_BASE_ADDRESS);
-	page_directory[KERNEL_PDE_ENTRY] = page_directory[KERNEL_PDE_ENTRY] | PDE_ENTRY_4MB_PRESENT;
-}
 
 /*
- * void init_rest_of_memory()
- * DESCRIPTION: initializes page descriptor entries to not present for 8MB to 4GBs
- * INPUT: none.
- * OUTPUTS: none.
- * RETURN VALUE: none.
- * SIDE EFFECTS: initializes page descriptor
+ * void setup_base_page_table(pte_t* base_page_table)
+ * DESCRIPTION: Sets up a "base" (0MB-4MB) page table, pointed to by base_page_table, marking all entries as not present with the exception of video memory
+ * INPUTS: base_page_table - pointer to base page table to set up
+ * OUTPUTS: base_page_talbe - updates this table to be configured properly
+ * RETURN VALUE: none
+ * SIDE EFFECTS: updates base_page_table
 */
-void init_rest_of_memory()
+void setup_base_page_table(pte_t* base_page_table)
 {
 	int i;
-	for(i = NOT_PRESENT_MEMORY_START; i < PD_ENTRIES; i++)
-	{
-		page_directory[i] = PDE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_4MB);
-		page_directory[i] = page_directory[i] | PDE_ENTRY_NOT_PRESENT;
+
+	// loop all entries of the page table. set the video memory entry to map to physical video memory, mark everything else as not present.
+	for(i=0; i < PT_ENTRIES; i++) {
+		if(i*BYTES_TO_ALIGN_TO == VIDEO_MEM_BASE) {
+			base_page_table[i] = PTE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO);
+			base_page_table[i] = base_page_table[i] | PTE_ENTRY_PRESENT;
+		} else {
+			base_page_table[i] = PTE_ADDRESS_ASSIGN(i*BYTES_TO_ALIGN_TO);
+			base_page_table[i] = base_page_table[i] | PTE_ENTRY_NOT_PRESENT;
+		}
 	}
 }
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -21,6 +21,7 @@
 #define PDE_ENTRY_4MB_NOT_PRESENT 0x00082			// defaults value only allow read/write
 #define PDE_ENTRY_PRESENT 0x000003
 #define PDE_ENTRY_NOT_PRESENT 0x000002				// defaults value only allow read/write
+#define PDE_USER_ACCESSIBLE 0x4 // setting this bit in a page directory entry enables user-mode access (i.e. ring3)
 
 /* General constants */
 #define PD_ENTRIES 1024
@@ -29,19 +30,24 @@
 #define BYTES_TO_ALIGN_4MB 0x400000
 #define NOT_PRESENT_MEMORY_START 2
 #define KERNEL_PDE_ENTRY 1
+#define MAX_TASKS 8
+
+#define TASK_VIRTUAL_BASE_ADDRESS 0x08000000 // (128MB)
+#define TASK_PROGRAM_IMAGE_OFFSET 0x00048000
 
 /* typedef to so that we can have some abstraction */
 typedef unsigned long pde_t;
 typedef unsigned long pte_t;
 
 /* externs the the page tables so that they can be used in other files */
-extern pde_t page_directory[PD_ENTRIES];
-extern pte_t page_table[PT_ENTRIES];
+extern pde_t kernel_page_directory[PD_ENTRIES];
+extern pte_t kernel_page_table[PT_ENTRIES];
 
 /* so that init function can be used in kernel.c */
 extern void init_paging();
 
 /* written in paging_asm.S */
 extern void paging_hw_enable(pde_t* base);
+extern void set_new_page_direcotry(pde_t* base);
 
 #endif
