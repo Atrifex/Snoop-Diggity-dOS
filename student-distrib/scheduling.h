bf61544a2ad652ddff409e1d8c03a8c77ed7ddb4
beaucha3 <beaucha3@illinois.edu>
2016-11-05 18:18:05 -0500

    Defined pcb structure

--- student-distrib/scheduling.h
+++ student-distrib/scheduling.h
@@ -1,4 +1,17 @@
 #ifndef SCHEDULING_H_
 #define SCHEDULING_H_
 
+#include "filesystem.h"
+
+#define MAX_FD_PER_PROCESS 8
+
+// Structure for Process Control Block
+typedef struct
+{
+	int32_t fd_array[MAX_FD_PER_PROCESS]; // File descriptors allocated for this process
+	uint32_t esp; // Stack pointer
+	uint32_t ebp; // Base pointer
+	uint8_t pid; // Process id
+} pcb_t;
+
 #endif
