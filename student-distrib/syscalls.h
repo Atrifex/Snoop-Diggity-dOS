fbe778ee9a69a72b0fcb9310add1a022d04c9960
beaucha3 <beaucha3@illinois.edu>
2016-11-07 22:08:51 -0600

    Jump tables for open/close/read/write

--- student-distrib/syscalls.h
+++ student-distrib/syscalls.h
@@ -6,6 +6,12 @@
 #include "filesystem.h"
 #include "keyboard.h"
 
+// Jump table types
+typedef asmlinkage int32_t(*open_func)(const uint8_t*);
+typedef asmlinkage int32_t(*close_func)(int32_t);
+typedef asmlinkage int32_t(*write_func)(int32_t, const void*, int32_t);
+typedef asmlinkage int32_t(*read_func)(int32_t, void*, int32_t);
+
 // System calls for our OS
 // Use asmlinkage to ensure arguments are passed via the stack (useful b/c assembly linkage)
 extern asmlinkage int32_t halt(uint8_t status);
@@ -16,6 +22,12 @@ extern asmlinkage int32_t open(const uint8_t* filename);
 extern asmlinkage int32_t close(int32_t fd);
 extern asmlinkage int32_t getargs(uint8_t* buf, int32_t num_bytes);
 extern asmlinkage int32_t vidmap(uint8_t** screen_start);
+
+// Jump tables
+open_func open_jmp_table[2] = {open_rtc, open_file};
+close_func close_jmp_table[2] = {close_rtc, close_file};
+write_func write_jmp_table[3] = {write_rtc, write_file, write_terminal};
+read_func read_jmp_table[3] = {read_rtc, read_file, read_terminal};
 
 // set_handler and sigreturn: TODO for extra credit
 
