61d4d52edc61eb9600f2f83efe4eba9891f621a9
beaucha3 <beaucha3@illinois.edu>
2016-11-07 23:09:01 -0600

    Changed typedefs structure

--- student-distrib/syscalls.h
+++ student-distrib/syscalls.h
@@ -6,11 +6,45 @@
 #include "filesystem.h"
 #include "keyboard.h"
 
-// Jump table types
+#define MAX_FD_PER_PROCESS 8
+
+// Jump table sub-types
 typedef asmlinkage int32_t(*open_func)(const uint8_t*);
 typedef asmlinkage int32_t(*close_func)(int32_t);
 typedef asmlinkage int32_t(*write_func)(int32_t, const void*, int32_t);
 typedef asmlinkage int32_t(*read_func)(int32_t, void*, int32_t);
+
+// Jump table type
+typedef struct
+{
+	open_func o_func;
+	close_func c_func;
+	read_func r_func;
+	write_func w_func;
+} file_operations_t;
+
+// Structure storing information about an open file
+typedef struct
+{
+	file_operations_t* jmp_table;
+	inode_t* file_inode;
+	int32_t file_position;
+	int32_t flags;
+} file_info_t;
+
+// Structure for Process Control Block
+typedef struct
+{
+	file_info_t fd_array[MAX_FD_PER_PROCESS]; // File descriptors allocated for this process
+	uint32_t esp0; // Parent's stack pointer
+	uint32_t esp; // Stack pointer
+	uint32_t ebp; // Base pointer
+	unsigned char* args; // Program's arguments
+	uint8_t pid; // Process id
+
+	// Maybe store IRET context of the parent in the child's PCB?
+	// Or not, because we jump to parent's execute in halt
+} pcb_t;
 
 // System calls for our OS
 // Use asmlinkage to ensure arguments are passed via the stack (useful b/c assembly linkage)
@@ -23,11 +57,10 @@ extern asmlinkage int32_t close(int32_t fd);
 extern asmlinkage int32_t getargs(uint8_t* buf, int32_t num_bytes);
 extern asmlinkage int32_t vidmap(uint8_t** screen_start);
 
-// Jump tables
-open_func open_jmp_table[2] = {open_rtc, open_file};
+/*open_func open_jmp_table[2] = {open_rtc, open_file};
 close_func close_jmp_table[2] = {close_rtc, close_file};
 write_func write_jmp_table[3] = {write_rtc, write_file, write_terminal};
-read_func read_jmp_table[3] = {read_rtc, read_file, read_terminal};
+read_func read_jmp_table[3] = {read_rtc, read_file, read_terminal};*/
 
 // set_handler and sigreturn: TODO for extra credit
 
