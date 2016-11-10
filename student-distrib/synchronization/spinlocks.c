f073ab086b8de70d49f0f3b7b54bf20d26af5f5b
Atrifex <rishirt.us@gmail.com>
2016-11-10 16:04:47 -0600

    Added spinlocks.c

---
+++ student-distrib/synchronization/spinlocks.c
@@ -0,0 +1,60 @@
+#include "spinlocks.h"
+
+/* /\**** Implement Later ****\/ */
+/* void spin_lock_init(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* int spin_is_locked(spinlock_t * lock) */
+/* { */
+/*     return 0; */
+/* } */
+
+/* int spin_trylock(spinlock_t * lock) */
+/* { */
+/*     return 0; */
+/* } */
+
+/* void spin_unlock_wait(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* /\**** Main Functions ****\/ */
+
+/* void spin_lock(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* void spin_unlock(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* #ifndef MULTIPROCESSOR_SYSTEM */
+
+/* void spin_lock_irqsave(spinlock_t * lock, unsigned long * flags) */
+/* { */
+/*     return; */
+/* } */
+
+/* void spin_unlock_irqrestore(spinlock_t * lock, unsigned long flags) */
+/* { */
+/*     return; */
+/* } */
+
+/* void spin_lock_irq(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* void spin_unlock_irq(spinlock_t * lock) */
+/* { */
+/*     return; */
+/* } */
+
+/* #else */
+/* /\* We don't need to have code specific to multiprocessor systems since we are working with a uniprocessor system *\/ */
+/* #endif */
